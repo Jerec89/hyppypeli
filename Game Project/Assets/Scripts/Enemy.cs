9776141e52d97b9cd691964d2a9b2a1434398b31
Jere89 <tacular_x@hotmail.com>
2015-05-27 14:40:57 +0300

    Commit 5

---
+++ Game Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float MoveSpeed;
+	public Vector2 MoveAmount;
+	private float MoveDirection = 1.0f;
+
+	bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		MoveAmount.x = MoveDirection * MoveSpeed * Time.deltaTime;
+		transform.Translate (MoveAmount);
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Wall1") {
+			MoveDirection = -1;
+			Flip();
+		} else if (coll.gameObject.tag == "Wall2"){
+			MoveDirection = 1;
+			Flip();
+		}
+	}
+
+	void Flip ()
+	{
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+
+
+}
--- Game Project/Assets/Scripts/PlayerControl.cs
+++ Game Project/Assets/Scripts/PlayerControl.cs
@@ -4,8 +4,9 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 	
-	[HideInInspector] public bool facingRight = true;
-	[HideInInspector] public bool jump = false;
+    bool facingRight = true;
+    bool jump = false;
+
 	public float moveForce;
 	public float maxSpeed;
 	public float jumpForce;
@@ -24,12 +25,20 @@ public class PlayerControl : MonoBehaviour {
 
 
 
+		if (rb2d.velocity.y > 0.0f) {
+			Physics2D.IgnoreLayerCollision (8, 9, true);
+		} else {
+			Physics2D.IgnoreLayerCollision(8, 9, false);
+		}
+
 		if (Input.GetButtonDown ("Jump")) {
-			if(hyppy){
-			jump = true;
+			if (hyppy) {
+				jump = true;
 				hyppy = false;
 			}
 		}
+		
+		
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
@@ -43,21 +52,40 @@ public class PlayerControl : MonoBehaviour {
 	void FixedUpdate(){
 
 			float h = Input.GetAxis("Horizontal");
-			
-			if (h * rb2d.velocity.x < maxSpeed)
-				rb2d.AddForce(Vector2.right * h * moveForce);
-			
-			if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
-				rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-			
-			
+
+			if (h * rb2d.velocity.x < maxSpeed) {
+
+			rb2d.AddForce (Vector2.right * h * moveForce);
+
+		}
+			if (Mathf.Abs (rb2d.velocity.x) > maxSpeed) {
+
+			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+		}
+
+		if (h > 0 && !facingRight) {
+			Flip ();
+		} else if (h < 0 && facingRight) {
+			Flip ();
+		}
+
 			if (jump)
 			{
 				rb2d.AddForce(new Vector2(0f, jumpForce));
 				jump = false;
 			}
+
+
 		}
 
+	void Flip ()
+	{
+		facingRight = !facingRight;
+		
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 		 
 	
 }
