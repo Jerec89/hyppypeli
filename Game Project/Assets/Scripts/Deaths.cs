b16850c98d476c717d99d6dbbfa3ac1883cb1feb
Jere89 <tacular_x@hotmail.com>
2015-07-03 15:40:50 +0300

    viimeiset kentät

--- Game Project/Assets/Scripts/Deaths.cs
+++ Game Project/Assets/Scripts/Deaths.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class Deaths : MonoBehaviour {
 
-	public string SortingLayerName = "Default";
-	public int SortingOrder = 0;
 	
 	TextMesh death;
 
@@ -16,9 +14,6 @@ public class Deaths : MonoBehaviour {
 		death = GetComponent <TextMesh> ();
 
 		deaths = 0;
-
-		gameObject.GetComponent<MeshRenderer> ().sortingLayerName = SortingLayerName;
-		gameObject.GetComponent<MeshRenderer> ().sortingOrder = SortingOrder;
 	}
 	
 	// Update is called once per frame
--- Game Project/Assets/Scripts/LevelChange.cs
+++ Game Project/Assets/Scripts/LevelChange.cs
@@ -175,20 +175,25 @@ public class LevelChange : MonoBehaviour {
 		}
 
 		else if (coll.gameObject.tag == "Player1" && gameObject.tag == "levelA" && Application.loadedLevelName == "Level4a2") {
-			Application.LoadLevel ("MainMenu");
+			Application.LoadLevel ("Level4a3");
 		}
 		
 		else if (coll.gameObject.tag == "Player1" && gameObject.tag == "levelB" && Application.loadedLevelName == "Level4a2") {
-			Application.LoadLevel ("MainMenu");
+			Application.LoadLevel ("Level4b3");
 		}
 
 		else if (coll.gameObject.tag == "Player1" && gameObject.tag == "levelA" && Application.loadedLevelName == "Level4b2") {
-			Application.LoadLevel ("MainMenu");
+			Application.LoadLevel ("Level4b3");
 		}
 		
 		else if (coll.gameObject.tag == "Player1" && gameObject.tag == "levelB" && Application.loadedLevelName == "Level4b2") {
+			Application.LoadLevel ("Level4a3");
+		}
+
+		else if (coll.gameObject.tag == "Player1" && gameObject.tag == "levelC" && (Application.loadedLevelName == "Level4a3" || Application.loadedLevelName == "Level4b3") ) {
 			Application.LoadLevel ("MainMenu");
 		}
+
 
 
 	}
